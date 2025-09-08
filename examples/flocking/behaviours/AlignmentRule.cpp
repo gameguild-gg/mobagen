dafa559b1814e2270b76d41abd4109e795713038
Traver <traver.blumenfeld@mymail.champlain.edu>
2025-09-08 18:47:33 -0400

    Implemented 2 rules
    Alignment and Separation look right but might need to adjust so they show up visually

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -4,7 +4,19 @@
 Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Try to match the heading of neighbors = Average velocity
   Vector2f averageVelocity = Vector2f::zero();
-
+  float boidsInRange = 0;
+  for (int i=0; i<neighborhood.size(); i++) {
+    if ((boid->getPosition() - neighborhood[i]->getPosition()).getMagnitude() <= boid->getDetectionRadius()) {
+       averageVelocity = averageVelocity + neighborhood[i]->getVelocity();
+      boidsInRange++;
+    }
+  }
+  if (boidsInRange!=0) {
+Vector2f avgForce = averageVelocity / boidsInRange;
+Vector2f forceApplied = boid->getVelocity() - avgForce;
+    return forceApplied;
+  }
+  return Vector2f::zero();
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
 
--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -9,16 +9,33 @@ Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Bo
 
   //    float desiredDistance = desiredMinimalDistance;
   //
-  //    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-  //    if (!neighborhood.empty()) {
-  //        Vector2f position = boid->transform.position;
-  //        int countCloseFlockmates = 0;
-  //        // todo: find and apply force only on the closest mates
-  //    }
+  // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
+      if (!neighborhood.empty()) {
+       Vector2f position = boid->transform.position;
+          int countCloseFlockmates = 0;
+          // todo: find and apply force only on the closest mates
+     }
 
   separatingForce = Vector2f::normalized(separatingForce);
+  Vector2 sepForce = {0.0,0.0};
+  double boidsInRangeCount = 0.0;
+  for (int i = 0; i < neighborhood.size(); ++i) {
+    if (neighborhood[i]==boid) {
+      Vector2 distance =(boid->getPosition() - neighborhood[i]->getPosition());
+      if (distance.getMagnitude() <= boid->getDetectionRadius() && distance.getMagnitude() > 0) {
+        Vector2 rep = distance.normalized() * getBaseWeightMultiplier()/distance.getMagnitude();
+        separatingForce += rep;
+        //Vector2 separation = {direction.x * k/(distance.x), direction.y * k/(distance.y)};
+        boidsInRangeCount += 1.0;
+      }
+    }
+  }
+   if (boidsInRangeCount!=0.0) {
 
-  return separatingForce;
+      Vector2 maxed = separatingForce.normalized();
+      return maxed;
+  }
+  return {0, 0};
 }
 
 bool SeparationRule::drawImguiRuleExtra() {
