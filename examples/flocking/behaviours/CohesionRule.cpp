b0b12046ec753b8ca8b7364b5098772ed45c05fd
Traver <traver.blumenfeld@mymail.champlain.edu>
2025-09-08 20:15:36 -0400

    Cohesion!
    Finally it looks like the gif, Now to add some completely other stuff to make it my own.

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -3,8 +3,20 @@
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   Vector2f cohesionForce;
-
-  // todo: add your code here to make a force towards the center of mass
+  float boidsInRange = 0.0f;
+for (int i = 0; i < neighborhood.size(); i++) {
+if (neighborhood[i]!=boid) {
+if ((neighborhood[i]->getPosition() - boid->getPosition()).getMagnitude() <= boid->getDetectionRadius()) {
+cohesionForce += neighborhood[i]->getPosition();
+  boidsInRange++;
+}
+}
+}
+if (boidsInRange!= 0) {
+  cohesionForce = (cohesionForce / boidsInRange);
+Vector2f force =cohesionForce-boid->getPosition();
+  return force.normalized();
+}
   // hint: iterate over the neighborhood
 
   // find center of mass
