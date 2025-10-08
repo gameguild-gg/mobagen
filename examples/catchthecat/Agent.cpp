4f1547bbed30af207628d1ba4ff6dda4e62a0c64
Traver <traver.blumenfeld@mymail.champlain.edu>
2025-10-08 10:56:22 -0400

    Almost there with my agent
    Implemented the while of GeneratePath and made a helper function of getVisitableNeighbors

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -4,11 +4,26 @@
 #include <queue>
 #include "World.h"
 using namespace std;
+std::vector<Point2D> Agent::getVisitableNeighbors(World* world, const Point2D& current, unordered_set<Point2D>& visited) {
+  auto sideOver2 = world->getWorldSideSize() / 2;
+  std::vector<Point2D> visitables;
+  std::vector<Point2D> changes = world->neighbors(current);
+  for (auto i:changes) {
+    Point2D hold = {current.x + i.x, current.y + i.y};
+    if (world->isValidPosition(hold)) {
+      continue;
+    }
+    if (!visited.contains(hold)) {
+      visitables.push_back(hold);
+    }
+  }
+  return visitables;
+}
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
   queue<Point2D> frontier;                   // to store next ones to visit
   unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
-  unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
+  unordered_set<Point2D> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
 
   // bootstrap state
   auto catPos = w->getCat();
@@ -18,15 +33,30 @@ std::vector<Point2D> Agent::generatePath(World* w) {
 
   while (!frontier.empty()) {
     // get the current from frontier
+    Point2D current = frontier.front();
+    frontier.pop();
+    frontierSet.erase(current);
     // remove the current from frontierset
     // mark current as visited
-    // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    visited.insert(current);
+    // getVisitableNeightbors(world, current, visited) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    vector<Point2D> neighs = getVisitableNeighbors(w, current, visited);
     // iterate over the neighs:
+    for (int i = 0;i<neighs.size();i++) {
+      if (!visited.contains(neighs[i])) {
+      cameFrom.insert({neighs[i], current});
+      frontier.push(neighs[i]);
+      frontierSet.insert(neighs[i]);
+      }
+    }
     // for every neighbor set the cameFrom
     // enqueue the neighbors to frontier and frontierset
     // do this up to find a visitable border and break the loop
+  if (w->catWinsOnSpace(current)) {
+    break;
   }
-
+  }
+  
   // if the border is not infinity, build the path from border to the cat using the camefrom map
   // if there isnt a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -1,6 +1,8 @@
 #ifndef AGENT_H
 #define AGENT_H
 #include "math/Point2D.h"
+
+#include <unordered_set>
 #include <vector>
 
 class World;
@@ -11,6 +13,7 @@ public:
 
   virtual Point2D Move(World*) = 0;
 
+  std::vector<Point2D> getVisitableNeighbors(World* world, const Point2D& current, std::unordered_set<Point2D>& visited);
   std::vector<Point2D> generatePath(World* w);
 };
 
