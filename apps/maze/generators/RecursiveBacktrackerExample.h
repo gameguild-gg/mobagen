0a0fa2129f65318c7e4be46bdff12cdc18da8384
Ethan Matte <ejoma05@gmail.com>
2026-09-14 13:20:30 -0400

    recursive maze is working

--- apps/maze/generators/RecursiveBacktrackerExample.h
+++ apps/maze/generators/RecursiveBacktrackerExample.h
@@ -7,12 +7,14 @@
 #include <map>
 #include <vector>
 
+
 class RecursiveBacktrackerExample : public MazeGeneratorBase {
 private:
   std::vector<Point2D> stack;
   std::map<int, std::map<int, bool>> visited;  // naive. not optimal
   Point2D randomStartPoint(World* world);
   std::vector<Point2D> getVisitables(World* w, const Point2D& p);
+  
 
 public:
   RecursiveBacktrackerExample() = default;
